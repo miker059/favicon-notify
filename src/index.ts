c3fa08e15059cffd050df21022e3196e55cd1eed
Mikhail <mihail@abrosk.in>
2021-12-17 14:09:13 +0300

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const FaviconNotify = (name: string) => `Hello ${name}`;
---
+++ tests/faviconNotify.spec.ts
@@ -0,0 +1,7 @@
+import { FaviconNotify } from '../src'
+
+describe('Test FaviconNotify', () => {
+  it('First test', () => {
+    expect('Hello Jenna').toBe(FaviconNotify('Jenna'))
+  })
+})
