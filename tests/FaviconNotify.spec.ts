5b4bc26fdb5bc63e03ba2a530bbbc6670d8bbbb2
Mikhail <mihail@abrosk.in>
2021-12-21 19:18:58 +0300

    fake test

---
+++ tests/FaviconNotify.spec.ts
@@ -0,0 +1,9 @@
+import FaviconNotify from '../src/index'
+
+const faviconNotify = new FaviconNotify()
+
+describe('FaviconNotify tests', () => {
+  it('blank', () => {
+    expect(true).toBe(true)
+  })
+})
